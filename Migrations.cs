1e6e05cd9926def24375b2a78c9cec58c3594a8d
Carl Woodhouse <nothing.is.forever@gmail.com>
2013-01-07 22:39:52 +0000

    started data migration

---
+++ Migrations.cs
@@ -0,0 +1,36 @@
+﻿using Orchard.ContentManagement.MetaData;
+using Orchard.Core.Contents.Extensions;
+using Orchard.Data.Migration;
+
+namespace Summit.Core {
+    public class UsersDataMigration : DataMigrationImpl {
+
+        public int Create() {
+            ContentDefinitionManager.AlterPartDefinition("HotelPart",
+                cfg => cfg
+                        .WithField("Description", field => field.OfType("TextField").WithDisplayName("Description").WithSetting("Flavor", "Html"))
+                        .WithField("Logo", field => field.OfType("ImageField").WithDisplayName("Logo"))
+                        .WithField("Phone", field => field.OfType("TextField").WithDisplayName("Phone Number"))
+                        .WithField("ReservationUrl", field => field.OfType("TextField").WithDisplayName("Reservation Url"))
+                        .WithField("Active", field => field.OfType("BooleanField").WithDisplayName("Active")));
+
+            ContentDefinitionManager.AlterTypeDefinition("Hotel",
+                 cfg => cfg
+                     .WithPart("CommonPart", p => p
+                        .WithSetting("DateEditorSettings.ShowDateEditor", "true"))
+                     .WithPart("PublishLaterPart")
+                     .WithPart("TitlePart")
+                     .WithPart("AutoroutePart", builder => builder
+                         .WithSetting("AutorouteSettings.AllowCustomPattern", "true")
+                         .WithSetting("AutorouteSettings.AutomaticAdjustmentOnEdit", "false")
+                         .WithSetting("AutorouteSettings.PatternDefinitions", "[{Name:'Title', Pattern: 'hotel/{Content.Slug}', Description: 'hotel/my-hotel'}]")
+                         .WithSetting("AutorouteSettings.DefaultPatternIndex", "0"))
+                     .WithPart("HotelPart")
+                     .Draftable()
+                     .Creatable()
+                 );
+
+            return 1;
+        }
+    }
+}
